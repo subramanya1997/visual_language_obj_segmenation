f6b3a64b3ccc845e35c32c2ea664da60bca1b9fa
snagabhushan_umass_edu <snagabhushan_umass_edu@login2.unity.rc.umass.edu>
2022-10-09 05:49:29 +0000

    Update to main and training files 1. Added a main file 2. Added a training file 3. Made changes to main file to run on single gpu.

---
+++ main.py
@@ -0,0 +1,48 @@
+import argparse
+import torch
+from trainer import Trainer
+import yaml
+import os
+import wandb
+
+def run(args):
+    with open(args.config_path) as f:
+        config = yaml.load(f, Loader=yaml.FullLoader)
+
+    config = {k: v['value'] for k, v in config.items()}
+    config = {**config, **vars(args)}
+    config = argparse.Namespace(**config)
+    trainer = Trainer(config)
+    if config.running_mode == 'train':
+        trainer.train()
+    else: #test mode
+        model_state_dict = torch.load(config.checkpoint_path)
+        if 'model_state_dict' in model_state_dict.keys():
+            model_state_dict = model_state_dict['model_state_dict']
+        trainer.model.load_state_dict(model_state_dict, strict=True)
+        trainer.evaluate()
+    
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser('MMVN training and evaluation')
+    parser.add_argument('--config_path', '-c', required=True,
+                        help='path to configuration file')
+    parser.add_argument('--running_mode', '-rm', choices=['train', 'eval'], required=True,
+                        help="mode to run, either 'train' or 'eval'")
+    parser.add_argument('--window_size', '-ws', type=int,
+                        help='window length to use during training/evaluation.'
+                             'note - in Refer-YouTube-VOS this parameter is used only during training, as'
+                             ' during evaluation full-length videos (all annotated frames) are used.')
+    parser.add_argument('--batch_size', '-bs', type=int, required=True,
+                        help='training batch size per device')
+    parser.add_argument('--eval_batch_size', '-ebs', type=int,
+                        help='evaluation batch size per device. '
+                             'if not provided training batch size will be used instead.')
+    parser.add_argument('--checkpoint_path', '-ckpt', type=str,
+                        help='path of checkpoint file to load for evaluation purposes')
+    args = parser.parse_args()
+
+    if args.eval_batch_size is None:
+        args.eval_batch_size = args.batch_size               
---
+++ trainer.py
@@ -0,0 +1,19 @@
+"""
+This file contains a Trainer class which handles the training and evaluation of MTTR.
+"""
+
+import math
+import sys
+import os
+from os import path
+
+Class Trainer:
+    def __init__(self, config):
+        self.config = config
+    
+    def train(self):
+        pass
+    
+    def evaluate(self):
+        pass
+        
